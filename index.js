f9f4939cc02689574a0b0bb930849b60513b77fb
Emily <aspentree79@gmail.com>
2019-06-19 19:17:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ function prependKitten(element) {
 }
 
 function removeLastKitten(array) {
-var newArray =  kittens.slice();
+var newArray =  kittens.slice(0, kittens.length - 1);
   return newArray
 }
